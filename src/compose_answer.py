4a5f27fb2cff82b36d4ae718510f2eddae130d3c
Lucas Soares <lucassoares1793@gmail.com>
2018-06-16 03:35:17 -0300

    Rename ans.py to compose_answer.py for more descriptive name

---
+++ src/compose_answer.py
@@ -0,0 +1,26 @@
+##############################################
+# Processamento de Linguagem Natural         #
+# SCC0633                                    #
+# General countries Chatbot main source file #
+#                                            #
+# Giovanna Oliveira Guimarães   9293693      #
+# Lucas Alexandre Soares        9293265      #
+# Rafael Joegs Monteiro         9293095      #
+# Darlan Xavier                 XXX          #
+#                                            #
+##############################################
+
+import sys
+
+# Replaces the question pronoun in order to 
+# compose the answer
+def _replace_pronoun(parsed_question, answer):
+	phrase = parsed_question.pergunta.replace("?", "")
+	ans = phrase.split(" ")
+
+	# x is a word of ans
+	final = [answer if x.lower() == parsed_question.pron else x for x in ans]
+
+	return print(" ".join(final))
+
+_replace_pronoun(parsed, answer)

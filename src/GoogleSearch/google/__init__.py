cd9c88ac9afebfeae0f7ae7a24a5a49229fffc9c
Lucas Soares <lucassoares1793@gmail.com>
2018-06-16 20:55:21 -0300

    Remove GoogleSearch moduel from project

--- src/GoogleSearch/google/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import
-from .modules import calculator, currency, images, utils
-from .modules import news_search, standard_search, shopping_search
-
--- src/GoogleSearch/google/modules/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import print_function
-from . import news_search
-from . import calculator
-from . import currency
-from . import images
-from . import shopping_search
-from . import standard_search
